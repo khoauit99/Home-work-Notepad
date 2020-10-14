2af713ed9c523dbd226c64aae81d695c6057baea
khoauit99 <61730743+khoauit99@users.noreply.github.com>
2020-10-14 23:53:57 +0700

    Update Replace.cs

--- WindowsFormsApp1/Replace.cs
+++ WindowsFormsApp1/Replace.cs
@@ -38,6 +38,21 @@ namespace WindowsFormsApp1
             fm3.replace_only(temp2);
             fm3.temp_findnext(temp);
         }
+        
+         private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string temp1 = string.Empty;
+            string temp2 = string.Empty;
+            temp1 = textBox1.Text.ToString();
+            temp2 = textBox2.Text.ToString();
+            Form1 fm3 = (Form1)this.Owner;
+            fm3.replace_all(temp1, temp2);
+        }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
